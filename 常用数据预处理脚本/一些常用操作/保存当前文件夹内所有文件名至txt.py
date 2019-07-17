bf45d5d8bb616cb4bae3d4ead25c94ea32c6f4a6
Henry <40362306+clw5180@users.noreply.github.com>
2019-07-17 21:55:00 +0800

    Update 保存当前文件夹内所有文件名至txt.py

--- 常用数据预处理脚本/一些常用操作/保存当前文件夹内所有文件名至txt.py
+++ 常用数据预处理脚本/一些常用操作/保存当前文件夹内所有文件名至txt.py
@@ -11,6 +11,6 @@ image_path = './annotations'
 with open('xmllist.txt', 'a+') as f:
     for filename in os.listdir(image_path):              #listdir的参数是文件夹的路径
         #print(filename)                                  #此时的filename是文件夹中文件的名称
-        f.write(filename + ' ')
+        f.write(filename + '\n')
 
 print('clw:end!')
