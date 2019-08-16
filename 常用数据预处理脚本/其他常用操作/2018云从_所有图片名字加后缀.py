0bdafade66ebded39d677777450a0b198bafefb7
Henry <40362306+clw5180@users.noreply.github.com>
2019-08-16 16:16:21 +0800

    Update 2018云从_所有图片名字加后缀.py

--- 常用数据预处理脚本/其他常用操作/2018云从_所有图片名字加后缀.py
+++ 常用数据预处理脚本/其他常用操作/2018云从_所有图片名字加后缀.py
@@ -1,8 +1,12 @@
 import os
 
-img_path = '/media/clwclw/data/2018yuncong/Part_B/train_data/images/'
+img_path = '/media/clwclw/data/2018yuncong/Part_B/train_data/'
 
+count = 0
 for filename in os.listdir(img_path): #listdir的参数是文件夹的路径4
     print(filename)
     assert filename.endswith(('jpg','png','jpeg','bmp'))  # 如果有不是图片的文件,报错
-    os.rename(os.path.join(img_path, filename), os.path.join(img_path, filename+'_Part_B'))
+    tmp_list = filename.split('.')
+    os.rename(os.path.join(img_path, filename), os.path.join(img_path, tmp_list[0] + '_Part_B' + tmp_list[1]))
+    count += 1
+    print('clw: image count = ', count)
